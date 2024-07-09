9e868eef54aea3e6e485494870dfbc4daaddcb27
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-07-09 22:22:52 +0300

    update-12
    study Parallel Routes, added widgets to dashboard

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import MagicButton from '@/app/components/magic-button';
 import React from 'react';
 
 export interface PageProps {}
@@ -7,7 +6,6 @@ export default function Page({}: PageProps) {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
-      <MagicButton />
     </main>
   );
 }
