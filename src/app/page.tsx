75e4a9e7842c3a59fddea8d2efb6baa9098271d1
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-07-13 15:39:42 +0300

    update-16
    added react Query, devtools, did refactoring code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import AddCompanyButton from './components/add-company-button';
+// import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home Page </h1>
-      <AddCompanyButton />
     </main>
   );
 }
